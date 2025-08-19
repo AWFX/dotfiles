4827d6cbac7969a0d07392c449b598e4c51a8e97
AWFX <94919004+AWFX@users.noreply.github.com>
2025-08-19 18:54:40 +0500

    Update configuraton.nix

--- nixos/configuraton.nix
+++ nixos/configuraton.nix
@@ -1,47 +1,49 @@
 { config, lib, pkgs, ...}:
 
+{
   imports = [
     ./hardware-configuration.nix
   ];
 
-# Use the systemd-boot EFI boot loader.
-boot.loader.systemd-boot.enable = true;
-boot.loader.efi.canTouchEfiVariables = true;
-
-# Networking.
-networking.hostName = "laptop";
-networking.networkmanager.enable = true;
-
-# Set time zone.
-time.timeZone = "Asia/Yekaterinburg";
-
-# User account.
-users.users.loylifer = {
-  isNormalUser = true;
-  extraGroups = [ "wheel" ];
-};
-
-nixpkgs.config.allowUnfree = true;
-programs.hyprland.enable = true;
-
-# Install pkgs.
-environment.systemPackages = with pkgs; [
-  neovim
-  wget
-  firefox
-  virtualbox
-  kitty
-  openssh
-  google-chrome
-];
-
-# Enable the OpenSSH daemon.
-services.openssh = {
-  enable = true;
-  settings = {
-    Port = 22;
-    PermitRootLogin = "yes";
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  
+  # Networking.
+  networking.hostName = "laptop";
+  networking.networkmanager.enable = true;
+  
+  # Set time zone.
+  time.timeZone = "Asia/Yekaterinburg";
+  
+  # User account.
+  users.users.loylifer = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
   };
-};
-
-system.stateVersion = "22.05";
+  
+  nixpkgs.config.allowUnfree = true;
+  programs.hyprland.enable = true;
+  
+  # Install pkgs.
+  environment.systemPackages = with pkgs; [
+    neovim
+    wget
+    firefox
+    virtualbox
+    kitty
+    openssh
+    google-chrome
+  ];
+  
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = true;
+    settings = {
+      Port = 22;
+      PermitRootLogin = "yes";
+    };
+  };
+  
+  system.stateVersion = "22.05";
+}
