cd3849a87b2ed0193fea56fb16a4bc2f1e38c415
AWFX <loylifer@gmail.com>
2025-08-26 19:21:39 +0500

    Good edit file

--- gns3-env.nix
+++ gns3-env.nix
@@ -12,43 +12,11 @@ pkgs.buildFHSEnv {
     qemu
     vpcs
     ubridge
-
-    # Базовые библиотеки
-    python3
-    udev
-    libvirt
-    openssl
-    iproute2
-    iptables
-    netcat
-    socat
-    bridge-utils
-    openvpn
   ];
 
   multiPkgs = pkgs: with pkgs; [
-    # 64-битные библиотеки для GUI и сетей
-    qt5.full
-    gtk3
-    cairo
-    pango
-    gdk-pixbuf
-    glibc
-    libGL
-    libGLU
-    libpcap
-    libuuid
-    zlib
-    bzip2
   ] ++ (with pkgs.pkgsi686Linux; [
-    # 32-битные аналоги, если понадобятся
-    libGL
-    libGLU
-    libpcap
-    zlib
-    bzip2
     glibc
-    djgpp_i686
   ]);
 
   runScript = "gns3";
